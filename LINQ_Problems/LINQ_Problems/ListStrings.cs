4bdeb4abc3bbf742367896d4f7839d21f932ee7a
tvang83 <62636069+tvang83@users.noreply.github.com>
2020-05-14 10:06:51 -0500

    update

---
+++ LINQ_Problems/LINQ_Problems/ListStrings.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Problems
+{
+    class ListStrings
+    {
+        //member variables 
+        List<String> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+        List<String> names = new List<string>() { "Mike", "Brad", "Nevin", "Ian", "Mike" };
+
+        //constructor
+
+        //methods
+
+        //problem 1 
+        public void GetChChar()
+        {
+            IEnumerable<char> words =
+            from ch in words
+            where Char(ch)
+
+        }
+
+        //problem 2 
+
+
+
+
+
+
+    }
+}
